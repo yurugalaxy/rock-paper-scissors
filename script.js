bfad9d45b402dfd8bf99478f073277bf090b5981
yurugalaxy <theblueberrywolfe@gmail.com>
2024-06-13 15:50:59 +0100

    add html buttons, js stuff

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
+const bestOf = 5;
+
 let itemArray = ["rock","paper","scissors"],
 scoreUser = 0,
-scoreCpu = 0;
+scoreCpu = 0,
+userNum = GetUserNum(prompt("a")),
+cpuNum = (max) => Math.floor(Math.random() * max);
 
-getWinner(getUserNum(prompt('Enter "rock:, "paper", or "scissors" to play :3')), getRandomInt(3));
+GetWinner(userNum, cpuNum(3));
 
-function getUserNum(userString) {
+function GetUserNum(userString) {
 let userComp = userString.toLowerCase();
+
         return itemArray.indexOf(userComp);
 }
 
-function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-}
-
-function getWinner(user, cpu) {
-        if (user != -1) {
-                switch(Math.abs(user - cpu)) {
-                        case 0:
-                                alert(`It's a tie! You both chose ${itemArray[user]}!`);
-                                break;
-                        case 1:
-                                compare(">", user, cpu);
-                                break;
-                        case 2:
-                                compare("<", user, cpu);
-                                break;
-                        default:
-                                break;
-                }
-        } else {
+function GetWinner(user, cpu) {
+        if (user == -1) {
                 alert("Enter the right thing ya dingus.");
+                return;
+        }
+
+        switch(Math.abs(user - cpu)) {
+                case 0:
+                        alert(`It's a tie! You both chose ${itemArray[user]}!`);
+                        break;
+                case 1:
+                        Compare(">", user, cpu);
+                        break;
+                case 2:
+                        Compare("<", user, cpu);
+                        break;
+                default:
+                        break;
         }
 }
 
-function compare(type, user, cpu) {
+function Compare(type, user, cpu) {
         let win = `You win! ${itemArray[user]} beats ${itemArray[cpu]}.`,
         lose = `You lose! ${itemArray[cpu]} beats ${itemArray[user]}.`,
         message;
+
                 if (type == "<") {
                         ++scoreUser;
                         message = (user < cpu) ? win : lose;
@@ -44,6 +47,7 @@ function compare(type, user, cpu) {
                         ++scoreCpu;
                         message = (user > cpu) ? win : lose;
                 };
+
         alert(message);
         alert(`It is currently ${scoreUser} to ${scoreCpu}.`);
 }
