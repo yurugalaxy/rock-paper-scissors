3a5410be62058fdd61dd6dcc2f86b52b3717eaa6
yurugalaxy <theblueberrywolfe@gmail.com>
2024-06-14 13:16:09 +0100

    refactor

--- script.js
+++ script.js
@@ -1,53 +1,186 @@
-const bestOf = 5;
+const bestOf = 5,
+choiceArray = ["rock","paper","scissors"];
 
-let itemArray = ["rock","paper","scissors"],
-scoreUser = 0,
-scoreCpu = 0,
-userNum = GetUserNum(prompt("a")),
-cpuNum = (max) => Math.floor(Math.random() * max);
+// let playerNum = 5,
+// opponentNum = 2;
+let playerNum = GetPlayerChoice(prompt("aaa")),
+opponentNum = Math.floor(Math.random() * 3),
+playerScore = 0,
+opponentScore = 0;
 
-GetWinner(userNum, cpuNum(3));
+function GetPlayerChoice(entry) {
+        let choice = entry.toLowerCase();
 
-function GetUserNum(userString) {
-let userComp = userString.toLowerCase();
-
-        return itemArray.indexOf(userComp);
+        return choiceArray.indexOf(choice);
 }
 
-function GetWinner(user, cpu) {
-        if (user == -1) {
-                alert("Enter the right thing ya dingus.");
+
+// BestWins(bestOf);
+// PlayRound(playerNum, opponentNum);
+
+function PlayRound(player, opponent) {
+        let result;
+
+        if (player === -1) {
+                alert("Enter the right things ya dingus")
                 return;
         }
 
-        switch(Math.abs(user - cpu)) {
+        switch (Math.abs(player - opponent)) {
                 case 0:
-                        alert(`It's a tie! You both chose ${itemArray[user]}!`);
+                        result = "tie";
                         break;
                 case 1:
-                        Compare(">", user, cpu);
+                        result = (player > opponent) ? "win" : "lose" ;
                         break;
                 case 2:
-                        Compare("<", user, cpu);
+                        result = (player < opponent) ? "win" : "lose" ;
+                        break;
+        }
+        DisplayResult(result, player, opponent);
+}
+
+
+function DisplayResult(result, player, opponent) {
+        let win = `You win! ${choiceArray[player]} beats ${choiceArray[opponent]}.`,
+        lose = `You lose! ${choiceArray[opponent]} beats ${choiceArray[player]}.`,
+        tie = `It's a tie! You both chose ${choiceArray[player]}!`,
+        message;
+
+        switch (result) {
+                case "win":
+                        message = win
+                        playerScore++;
+                        break;
+                case "lose":
+                        message = lose;
+                        opponentScore++;
                         break;
                 default:
+                        message = tie;
                         break;
+        }
+        alert(message + "\nYou:" + playerScore + " Opp:" + opponentScore);
+}
+
+function BestWins(bestof) {
+        let bestofCalc = bestof / 2 + 1;
+
+        while (playerScore < bestofCalc || opponentScore < bestofCalc) {
+                PlayRound(playerNum, opponentNum);
         }
 }
 
-function Compare(type, user, cpu) {
-        let win = `You win! ${itemArray[user]} beats ${itemArray[cpu]}.`,
-        lose = `You lose! ${itemArray[cpu]} beats ${itemArray[user]}.`,
-        message;
 
-                if (type == "<") {
-                        ++scoreUser;
-                        message = (user < cpu) ? win : lose;
-                } else {
-                        ++scoreCpu;
-                        message = (user > cpu) ? win : lose;
-                };
 
-        alert(message);
-        alert(`It is currently ${scoreUser} to ${scoreCpu}.`);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function PlayRound(player, opponent) {
+//         let type;
+
+//         if (player == -1) {
+//                 alert("Enter the right thing ya dingus.");
+//                 return;
+//         }
+
+//         switch(Math.abs(player - opponent)) {
+//                 case 0:
+//                         type = "="
+//                         break;
+//                 case 1:
+//                         type = ">"
+//                         break;
+//                 case 2:
+//                         type = "<"
+//                         break;
+//                 default:
+//                         break;
+//         }
+//         GetWinner(type, player, opponent);
+// }
+
+// function Compare(player, opponent) {
+//         let type;
+
+//         if (player == -1) {
+//                 alert("Enter the right thing ya dingus.");
+//                 return;
+//         }
+
+//         switch(Math.abs(player - opponent)) {
+//                 case 0:
+//                         type = "="
+//                         break;
+//                 case 1:
+//                         type = ">"
+//                         break;
+//                 case 2:
+//                         type = "<"
+//                         break;
+//                 default:
+//                         break;
+//         }
+//         return type;
+// }
+
+// function Win() {
+//         return;
+// }
+
+// function Lose() {
+//         return;
+// }
+
+// function Tie() {
+//         return;
+// }
+
+// function GetWinner(type, player, opponent) {
+//         let win = `You win! ${choiceArray[player]} beats ${choiceArray[opponent]}.`,
+//         lose = `You lose! ${choiceArray[opponent]} beats ${choiceArray[player]}.`,
+//         tie = `It's a tie! You both chose ${choiceArray[player]}!`,
+//         message;
+
+//         switch (type) {
+//                 case "=":
+//                         message = tie;
+//                         break;
+//                 case "<":
+//                         message = (player < opponent) ? win : lose;
+//                         break;
+//                 case ">":
+//                         message = (player > opponent) ? win : lose;
+//                         break;
+//                 default:
+//                         break;
+//         }
+
+//         return message;
+// }
