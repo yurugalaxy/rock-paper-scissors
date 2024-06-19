084946167dbecc6175f793eab9fa39772cb91ddd
yurugalaxy <theblueberrywolfe@gmail.com>
2024-06-19 13:13:04 +0100

    rework js to use GUI
    +added css style
    +js now uses a gui on the html
    +winner now a boolean
    -tie leaves winner as undefined, could be bad?

--- script.js
+++ script.js
@@ -1,80 +1,118 @@
-const bestOf = rounds.value,
-choiceArray = ["rock","paper","scissors"],
+const choiceArray = ["rock","paper","scissors"],
 divResult = document.querySelector(".Result"),
 divScore = document.querySelector(".Score"),
-playButtons = document.querySelectorAll(".mPlayButton");
+playButtons = document.querySelectorAll(".mPlayButton"),
+resetButton = document.querySelector(".Reset"),
+selectDrop = document.querySelector("#BestOfSelect");
 
 let playerScore = 0,
 opponentScore = 0;
 
-function BestWins(bestof) {
-        let bestofCalc = Math.floor(bestof / 2) + 1;
+selectDrop.addEventListener("change", (resetRounds));
 
-        while (playerScore < bestofCalc && opponentScore < bestofCalc) {
-                PlayRound();
-        }
+resetButton.addEventListener("click", (resetRounds));
 
-        if (playerScore > opponentScore) {
-                alert(`You won ${playerScore} to ${opponentScore}!`);
-        } else {
-                alert(`You lost ${opponentScore} to ${playerScore}!`);
-        }
+function resetRounds() {
+        playerScore = 0;
+        opponentScore = 0;
+        divResult.textContent = "";
+        divScore.textContent = "";
+        divResult.classList.remove("mWinText");
+        divResult.classList.remove("mLoseText");
+        divScore.classList.remove("mWinText");
+        divScore.classList.remove("mLoseText");
 }
 
 playButtons.forEach((button) => {
         button.addEventListener("click", () => {
-                PlayRound(button.id)
+                let currRound = PlayRound(button.id),
+                bestOf =
+                Math.floor(selectDrop.value /
+                2) + 1;
+
+                if (playerScore < bestOf && opponentScore < bestOf) {
+                        DisplayResult(
+                                GetWinner(
+                                        currRound.result,
+                                        currRound.player,
+                                        currRound.opponent
+                                )
+                        );
+                }
+
+        if (playerScore >= bestOf) {
+                divScore.classList.remove("mLoseText");
+                divScore.classList.add("mWinText");
+                divScore.textContent =
+                `Congratumalations! You won ` +
+                `${playerScore} to ${opponentScore}!`;
+        } else if (opponentScore >= bestOf) {
+                divScore.classList.remove("mWinText");
+                divScore.classList.add("mLoseText");
+                divScore.textContent =
+                `Oh heck. ` +
+                `You lost ${opponentScore} to ${playerScore}!`;
+        }
         });
 });
 
 function PlayRound(inputPlayer) {
-        let result,
+        let win,
         opponent = Math.floor(Math.random() * 3),
         player = choiceArray.indexOf(inputPlayer);
 
         switch (Math.abs(player - opponent)) {
-                case 0:
-                        result = "tie";
+                case 0: // Tie
+                        return {
+                                player: player,
+                                opponent: opponent
+                        }
+                case 1: // Largest number wins
+                        win = player > opponent;
                         break;
-                case 1:
-                        result = (player > opponent) ? "win" : "lose" ;
-                        break;
-                case 2:
-                        result = (player < opponent) ? "win" : "lose" ;
+                case 2: // Smallest number wins
+                        win = player < opponent;
                         break;
         }
-        return {res: result, pl: player, opp: opponent};
-        DisplayResult(result, player, opponent);
+        return {
+                result: win,
+                player: player,
+                opponent: opponent
+        };
 }
 
-function DisplayResult(result, player, opponent) {
-        let win =
+function GetWinner(win, player, opponent) {
+        let messWin =
         `You win! ${choiceArray[player]} beats ${choiceArray[opponent]}.`,
-        lose =
-        `You lose! ${choiceArray[opponent]} beats ${choiceArray[player]}.`,
-        tie =
+        messLose =
+        `You lose! ${choiceArray[player]} loses to ${choiceArray[opponent]}.`,
+        messTie =
         `It's a tie! You both chose ${choiceArray[player]}!`,
         message;
 
-        switch (result) {
-                case "win":
-                        divResult.classList.remove("mLoseText");
-                        divResult.classList.add("mWinText");
-                        message = win
-                        playerScore++;
-                        break;
-                case "lose":
-                        divResult.classList.remove("mWinText");
-                        divResult.classList.add("mLoseText");
-                        message = lose;
-                        opponentScore++;
-                        break;
-                default:
-                        divResult.classList.remove("mWinText");
-                        divResult.classList.remove("mLoseText");
-                        message = tie;
-                        break;
+        if (typeof win == 'undefined') {
+                divResult.classList.remove("mWinText");
+                divResult.classList.remove("mLoseText");
+                message = messTie;
+                return message;
         }
+
+        if (win) {
+                divResult.classList.remove("mLoseText");
+                divResult.classList.add("mWinText");
+                message = messWin
+                playerScore++;
+        } else {
+                divResult.classList.remove("mWinText");
+                divResult.classList.add("mLoseText");
+                message = messLose;
+                opponentScore++;
+        }
+
+        return message;
+}
+
+function DisplayResult(message) {
         divResult.textContent = message;
         divScore.textContent =
         "You:" + playerScore + " Opponent:" + opponentScore;
